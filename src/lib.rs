7c113d2db396fb90d3ee9bf9aafd298433baf37e
binhmod <165454425+binhmod@users.noreply.github.com>
2026-07-30 13:42:06 +0700

    Fix JNI environment pointer casting and update assembly

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use dobby_rs::Address;
 use jni::JNIEnv;
 use log::{error, info, trace};
 use nix::{fcntl::OFlag, sys::stat::Mode};
+// use std::arch::asm;
 use std::arch::naked_asm;
 use std::{
     fs::File,
@@ -16,20 +17,25 @@ struct MyModule {
 }
 
 impl Module for MyModule {
-    fn new(api: Api, env: *mut jni::sys::JNIEnv) -> Self {
+    fn new(api: Api, env: *mut jni_sys::JNIEnv) -> Self {
         android_logger::init_once(
             android_logger::Config::default()
                 .with_max_level(log::LevelFilter::Info)
                 .with_tag("dump_dex"),
         );
-        let env = unsafe { JNIEnv::from_raw(env).unwrap() };
+        let env = unsafe { JNIEnv::from_raw(env.cast()).unwrap() };
         Self { api, env }
     }
     fn pre_app_specialize(&mut self, args: &mut AppSpecializeArgs) {
         let mut inner = || -> anyhow::Result<()> {
             let package_name = self
                 .env
-                .get_string(&unsafe { jni::objects::JString::from_raw(*args.nice_name) })?
+                .get_string(unsafe {
+                    (args.nice_name as *mut jni_sys::jstring as *mut ()
+                        as *const jni::objects::JString<'_>)
+                        .as_ref()
+                        .unwrap()
+                })?
                 .to_string_lossy()
                 .to_string();
             trace!("pre_app_specialize: package_name: {}", package_name);
@@ -58,7 +64,7 @@ impl Module for MyModule {
                 return Ok(());
             }
             info!("dump {}", package_name);
-            let open_common = dobby_rs::resolve_symbol("libdexfile.so", "_ZN3art13DexFileLoader10OpenCommonENSt3__110shared_ptrINS_16DexFileContainerEEEPKhmRKNS1_12basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEENS1_8optionalIjEEPKNS_10OatDexFileEbbPSC_PNS_22DexFileLoaderErrorCodeE")
+            let open_common = dobby_rs::resolve_symbol("libdexfile.so", "_ZN3art13DexFileLoader10OpenCommonEPKhmS2_mRKNSt3__112basic_stringIcNS3_11char_traitsIcEENS3_9allocatorIcEEEEjPKNS_10OatDexFileEbbPS9_NS3_10unique_ptrINS_16DexFileContainerENS3_14default_deleteISH_EEEEPNS0_12VerifyResultE")
                 .ok_or_else(|| anyhow::anyhow!("resolve symbol error"))?;
             info!("open_common addr: {:x}", open_common as usize);
             unsafe {
@@ -87,30 +93,31 @@ static mut OLD_OPEN_COMMON: usize = 0;
 pub extern "C" fn new_open_common_wrapper() {
     naked_asm!(
         r#"
-            sub sp, sp, 0x280
-            stp x29, x30, [sp, #0]
-            stp x0, x1, [sp, #0x10]
-            stp x2, x3, [sp, #0x20]
-            stp x4, x5, [sp, #0x30]
-            stp x6, x7, [sp, #0x40]
-            stp x8, x9, [sp, #0x50]
+        sub sp, sp, 0x280
+        stp x29, x30, [sp, #0]
+        stp x0, x1, [sp, #0x10]
+        stp x2, x3, [sp, #0x20]
+        stp x4, x5, [sp, #0x30]
+        stp x6, x7, [sp, #0x40]
+        stp x8, x9, [sp, #0x50]
 
-            mov x0, x1
-            mov x1, x2
-            bl {new_open_common}
+        mov x0, x1
+        mov x1, x2
+        bl {new_open_common}
 
-            ldp x29, x30, [sp, #0]
-            ldp x0, x1, [sp, #0x10]
-            ldp x2, x3, [sp, #0x20]
-            ldp x4, x5, [sp, #0x30]
-            ldp x6, x7, [sp, #0x40]
-            ldp x8, x9, [sp, #0x50]
-            add sp, sp, 0x280
-            adrp x16, {old_open_common}
-            ldr x16, [x16, #:lo12:{old_open_common}]
-            br x16"#,
+        ldp x29, x30, [sp, #0]
+        ldp x0, x1, [sp, #0x10]
+        ldp x2, x3, [sp, #0x20]
+        ldp x4, x5, [sp, #0x30]
+        ldp x6, x7, [sp, #0x40]
+        ldp x8, x9, [sp, #0x50]
+        add sp, sp, 0x280
+        adrp x16, {old_open_common}
+        ldr x16, [x16, #:lo12:{old_open_common}]
+        br x16"#,
         new_open_common = sym new_open_common,
         old_open_common = sym OLD_OPEN_COMMON,
+        // options(noreturn)
     );
 }
 
